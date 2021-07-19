e3fde978e6e6d82e71272fb81e1180c6c8c0fa8c
Grivov7858 <grivov7858@gmail.com>
2021-07-19 19:18:04 +0300

    Initial commit

---
+++ src/main/java/com/qtech/orderoengine/OrderoEngineApplication.java
@@ -0,0 +1,13 @@
+package com.qtech.orderoengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderoEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderoEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qtech/orderoengine/OrderoEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qtech.orderoengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderoEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
